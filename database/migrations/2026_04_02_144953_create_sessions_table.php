a0422c362e0486b41fe0aea1afadc2fbef9c202b
azkalbhqi <azkalpredator@gmail.com>
2026-04-02 21:51:07 +0700

    add session table

---
+++ database/migrations/2026_04_02_144953_create_sessions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sessions', function (Blueprint $user_id) {
+            $user_id->string('id')->primary();
+            $user_id->foreignId('user_id')->nullable()->index();
+            $user_id->string('ip_address', 45)->nullable();
+            $user_id->text('user_agent')->nullable();
+            $user_id->longText('payload');
+            $user_id->integer('last_activity')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sessions');
+    }
+};
